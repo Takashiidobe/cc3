a2fb7547f1bf1b75198709ba6f73add1d5b981f1
Takashiidobe <idobetakashi@gmail.com>
2026-01-04 14:16:01 -0500

    Add __func__ built-in variable

--- src/parser.rs
+++ src/parser.rs
@@ -419,6 +419,12 @@ impl<'a> Parser<'a> {
 
         self.expect_punct(Punct::LBrace)?;
 
+        let mut func_bytes = name.as_bytes().to_vec();
+        func_bytes.push(0);
+        let func_ty = Type::array(Type::Char, func_bytes.len() as i32);
+        let func_idx = self.new_string_literal(func_bytes, func_ty);
+        self.push_scope_var_named("__func__".to_string(), func_idx, false);
+
         let mut body = self.parse_block_items()?;
         self.leave_scope();
         self.current_fn_return = prev_return;
